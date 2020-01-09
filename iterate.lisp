f0b4bed6d8ab0846fb13709d637b83437f782105
Paul F. Dietz <paul.f.dietz@gmail.com>
2020-01-08 20:47:22 -0500

    The code for #L reader macros must be available even when this file is loaded, as it the readtable is needed for SBCL's sb-cover:report to properly print coverage reports.

--- iterate.lisp
+++ iterate.lisp
@@ -462,7 +462,7 @@
 ;;; e.g. #L(list !2 !3 !5) is equivalent to:
 ;;;      (lambda (!1 !2 !3 !4 !5) (declare (ignore !1 !4)) (list !2 !3 !5))
 
-(eval-when (:compile-toplevel :execute)
+(eval-when (:compile-toplevel :load-toplevel :execute)
 
   (defun sharpL-reader (stream subchar n-args)
     (declare (ignore subchar))
